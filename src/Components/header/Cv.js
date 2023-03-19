c69bd5eba31567aa6ca766b2125d33e146c20d02
Bhavishy M <bhavisshyya@Bhavishys-MacBook-Air.local>
2023-03-19 20:30:38 +0530

    resume

--- src/Components/header/Cv.js
+++ src/Components/header/Cv.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Cv() {
   return (
     <div className='Cv'>
-        <a href='https://drive.google.com/file/d/1VSk3Bx7FRA312cxQ8wf6r8sFInX91s9j/view?usp=sharing' download className='btn'>Download Resume</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      <a
+        href='https://drive.google.com/file/d/1ug13BQbzahUCxrpuZ_GV0IBevjkEefIP/view?usp=sharing'
+        download
+        className='btn'
+      >
+        Download Resume
+      </a>
+      <a href='#contact' className='btn btn-primary'>
+        Let's Talk
+      </a>
     </div>
-  )
+  );
 }
 
-export default Cv
+export default Cv;
