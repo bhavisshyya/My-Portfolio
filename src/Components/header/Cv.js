9793a0466d25ffbd6f17978ee3919ea21bbbc5c1
Bhavishya Malhotra <105254969+bhavisshyya@users.noreply.github.com>
2024-06-06 21:51:58 +0530

    Update Cv.js

--- src/Components/header/Cv.js
+++ src/Components/header/Cv.js
@@ -4,7 +4,7 @@ function Cv() {
   return (
     <div className='Cv'>
       <a
-        href='https://drive.google.com/file/d/1XxvgudKYLF5EWjZQ8Fgnz7BdbxvQjCAo/view?usp=sharing'
+        href='https://drive.google.com/file/d/1MOi74z6_6uxdKIm1s45B4PmUfqxVevyK/view?usp=sharing'
         download
         className='btn'
       >
